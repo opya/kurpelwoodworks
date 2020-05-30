baf6590336c7a05d11c343fcb14ada231f1d2f47
Simeon Angelov <chepashnikow@gmail.com>
2020-05-30 15:52:43 +0300

    small tweeks
        - check for existing dir before try to get dir entries
        - center menu in layout.erb

--- www/entities/images.rb
+++ www/entities/images.rb
@@ -3,12 +3,19 @@ class Images
   TEMPLATES_PATH = "/assets/images".freeze
 
   class << self
+
     def for_project(name)
-      images = Dir.entries("#{BASE_PROJECT_IMAGES_PATH}#{name}")
-      images.sort!
-      images.reject!{ |i| i == '.' || i == '..' }
-      images.collect!{ |i| "#{TEMPLATES_PATH}/#{name}/#{i}" }
-      images
+      [].tap do |i|
+        if File.exist?("#{BASE_PROJECT_IMAGES_PATH}#{name}")
+          images = Dir.entries("#{BASE_PROJECT_IMAGES_PATH}#{name}")
+          images.sort!
+          images.reject!{ |i| i == '.' || i == '..' }
+          images.collect!{ |i| "#{TEMPLATES_PATH}/#{name}/#{i}" }
+
+          i << images unless images.empty?
+        end
+      end.flatten
     end
+
   end
 end
