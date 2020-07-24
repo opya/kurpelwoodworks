ddc5da9378f8ac64fff4ceea4760e88d14ec4e69
Simeon Angelov <chepashnikow@gmail.com>
2020-07-24 14:21:19 +0300

    shuffle

---
+++ www/entities/project/project_images.rb
@@ -0,0 +1,54 @@
+require 'mini_magick'
+require 'fileutils'
+
+class ProjectImage < Sequel::Model
+  many_to_one :project
+
+  BASE_IMAGES_PATH = "public/assets/images/"
+  TEMPLATES_PATH = "/assets/images"
+  CASHE_DIR = "cache/"
+  SCALE_PERCENT = "10%"
+  THUMBNAIL_PATH = ""
+
+  class << self
+    def for_project(project)
+      if File.exist?("#{BASE_IMAGES_PATH}#{project.work_name}")
+        images = Dir.entries("#{BASE_IMAGES_PATH}#{project.work_name}")
+        images.sort!
+        images.reject!{ |i| i == '.' || i == '..' || i == 'cache'}
+
+        images.each do |image_name|
+          ProjectImage.new(project: project, name: image_name).save
+        end
+      end
+    end
+  end
+
+  def before_save
+    generate_thumbnail!
+    super
+  end
+
+  private
+
+  def generate_thumbnail!
+    image = MiniMagick::Image.open("#{BASE_IMAGES_PATH}#{project.work_name}/#{name}")
+
+    self.path = "#{TEMPLATES_PATH}/#{project.work_name}/#{File.basename(name)}"
+    self.width = image.width
+    self.height = image.height
+
+    unless File.exist?("#{BASE_IMAGES_PATH}#{project.work_name}/#{CASHE_DIR}")
+      FileUtils.mkdir_p("#{BASE_IMAGES_PATH}#{project.work_name}/#{CASHE_DIR}")
+    end
+
+    #NOTE: regenerate thumbnail each time
+    thumbnail = image
+    thumbnail.resize(SCALE_PERCENT)
+    thumbnail.write("#{BASE_IMAGES_PATH}#{project.work_name}/#{CASHE_DIR}#{name}")
+
+    self.thumbnail_path = "#{TEMPLATES_PATH}/#{project.work_name}/#{CASHE_DIR}#{name}"
+    self.thumbnail_width = thumbnail.width
+    self.thumbnail_height = thumbnail.height
+  end
+end
---
+++ www/entities/project/project_mandoc_template.rb
@@ -0,0 +1,24 @@
+require 'open3'
+
+class ProjectMandocTemplate
+  BASE_TEMPLATE = <<-BODY
+.Dd $Mdocdate$
+.Dt PROGNAME section
+.Os
+.Sh Име
+.Nm %s
+%s
+  BODY
+
+  class << self
+    def for_project(project)
+      mandoc_template = sprintf(
+        BASE_TEMPLATE,
+        project.name,
+        project.description)
+      sin, sout, serr = Open3.popen3("echo '#{mandoc_template}' | mandoc -T html -O style=/assets/mandoc.css")
+
+      sout.read
+    end
+  end
+end
