8ae9cad8d221b870161e42199388e6060e878190
Simeon Angelov <chepashnikow@gmail.com>
2020-08-16 16:59:17 +0300

    and again ..

--- www/config/puma.rb
+++ www/config/puma.rb
@@ -6,7 +6,7 @@ env = ENV['RACK_ENV'] || 'development'
 
 if env == 'production'
   app_dir = '/var/www/htdocs/kurpelwoodworks.com/current/www'
-  pid_file = '/var/www/htdocs/kurpelwoodworks.com/shared/tmp/pima.pid'
+  pid_file = '/var/www/htdocs/kurpelwoodworks.com/shared/tmp/puma.pid'
 
   environment 'production'
   directory app_dir
